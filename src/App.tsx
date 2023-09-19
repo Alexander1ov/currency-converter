e463753215d9d3d24a02cfb46775aac6955633f2
Alexander1ov <pervov0993@gmail.com>
2023-09-19 11:39:40 +0300

    Basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { FC, useEffect } from "react";
 import { useAppDispatch } from "./hooks/hooks";
 import { fetchCurrency } from "./store/currency/currencySlice";
 
